8978b4962afdcf37d58f512568d9f253cf29386f
ICA\rc02018 <jan.blonde@digipolis.be>
2019-07-05 16:10:56 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,60 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// class Aggregate extends Component {
+//   render () {
+//     return (
+//       <div><h2>Number and a text</h2></div>
+//     );
+//   }
+// }
+
+let defaultTextColor = '#ffffff';
+
+let defaultStyle = {
+  color: defaultTextColor
+}
+
+function Aggregate(){
+  return(
+    <div style={{width: "40%", display: "inline-block"}}>
+      <h2 style={defaultStyle}>Number and a text</h2>
+    </div>
+  )
+}
+
+function Filter(){
+  return(
+    <div style={defaultStyle}>
+      <img></img>
+      <input type="text"/>
+      Filter
+    </div>
+  )
+}
+
+function PlayList(){
+  return(
+    <div style={{...defaultStyle, width:"25%", display: "inline-block"}}>
+      <img/>
+      <h3>Playlist Name</h3>
+      <ul><li>Song1</li><li>Song2</li><li>Song3</li></ul>
+
+    </div>
+  )
+}
+
 function App() {
-  return (
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Title</h1>
+      <Aggregate></Aggregate>
+      <Aggregate></Aggregate>
+      <Filter></Filter>
+      <PlayList/>
+      <PlayList/>
+      <PlayList/>
+      <PlayList/>
     </div>
   );
 }
